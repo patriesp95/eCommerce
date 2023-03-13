104f34b0a5c23c212659758424f2ba0c2c77ef45
Patricia Martinez Espert <patrimespert@outlook.com>
2023-03-13 16:31:59 +0100

    setup bottom nav view

--- app/src/main/java/com/patriciamespert/ecommerce/MainActivity.kt
+++ app/src/main/java/com/patriciamespert/ecommerce/MainActivity.kt
@@ -2,10 +2,42 @@ package com.patriciamespert.ecommerce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavGraph
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.patriciamespert.ecommerce.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fMain) as NavHostFragment?
+        val navController = navHostFragment?.navController
+
+        binding.bnvMainMenu.apply {
+            navController?.let { navController ->
+                NavigationUI.setupWithNavController(
+                    this,
+                    navController
+                )
+                setOnItemSelectedListener { item ->
+                    NavigationUI.onNavDestinationSelected(item, navController)
+                    true
+                }
+                setOnItemReselectedListener {
+                    val selectedMenuItemNavGraph =
+                        navController.graph.findNode(it.itemId) as NavGraph
+                    selectedMenuItemNavGraph.let { menuGraph ->
+
+                        navController.popBackStack(menuGraph.startDestinationId, false)
+                    }
+                }
+            }
+        }
     }
 }
